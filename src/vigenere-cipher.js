4ac84e280db073c167b3cddd4bf836636cad4d1b
laperson <43199823+laperson@users.noreply.github.com>
2023-04-15 03:04:15 +0300

    feat: done 1 task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(mod) {
+    this.mod = mod == undefined ? true : mod;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(message, key) {
+    if (key == undefined || message == undefined) throw new Error('Incorrect arguments!')
+    let y = 0, arr = [];
+    for (let i = 0; i < message.length; i++) {
+      if (key[y] == undefined) y = 0;
+      if (message[i].toUpperCase().charCodeAt() < 65 || message[i].toUpperCase().charCodeAt() > 90 ) {
+        arr.push(message[i]);
+        continue;
+      }
+      let m = message[i].toUpperCase().charCodeAt();
+      let k = key[y].toUpperCase().charCodeAt();
+      arr.push(65 + ((m + k) % 26));
+      y++;
+    }
+    return this.mod ? arr.map((a)=> typeof a === 'number' ? String.fromCharCode(a) : a).join('')
+                    : arr.map((a)=> typeof a === 'number' ? String.fromCharCode(a) : a).reverse().join('');
+  
+  }
+  decrypt(eMessage, key) {
+    if (key == undefined || eMessage == undefined) throw new Error('Incorrect arguments!')
+    let y = 0, arr = [];
+    for (let i = 0; i < eMessage.length; i++) {
+      if (key[y] == undefined) y = 0;
+      if (eMessage[i].toUpperCase().charCodeAt() < 65 || eMessage[i].toUpperCase().charCodeAt() > 90 ) {
+        arr.push(eMessage[i]);
+        continue;
+      }
+      let m = eMessage[i].toUpperCase().charCodeAt();
+      let k = key[y].toUpperCase().charCodeAt();
+  
+      arr.push(65 + (m - k + 26) % 26);
+      y++;
+    }
+    return this.mod ? arr.map((a)=> typeof a === 'number' ? String.fromCharCode(a) : a).join('')
+                    : arr.map((a)=> typeof a === 'number' ? String.fromCharCode(a) : a).reverse().join('');
   }
 }
+
 
 module.exports = {
   VigenereCipheringMachine
